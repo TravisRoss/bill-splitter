e2f31ab46f8a97c5862babeebc8d42c5225d86e4
Travis Ross <travis.ross@gira.de>
2025-11-29 17:56:15 +0100

    feat: build FriendsList and Friend components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+let friends = [
+  { id: 1, name: "John", emoji: "🤩", selected: false },
+  { id: 2, name: "Bale", emoji: "👶", selected: false },
+  { id: 3, name: "Ramsey", emoji: "🕶️", selected: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FriendsList />
+    </div>
+  );
+}
+
+function FriendsList() {
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleSelectFriend(friendId) {
+    setSelectedId(friendId === selectedId ? null : friendId);
+  }
+
+  return (
+    <div className="friend-list">
+      {friends.map((friend) => (
+        <Friend
+          name={friend.name}
+          emoji={friend.emoji}
+          selected={selectedId === friend.id}
+          onSelect={() => handleSelectFriend(friend.id)}
+        />
+      ))}
+      <AddFriendButton>Add friend</AddFriendButton>
+    </div>
+  );
+}
+
+function Friend({ name, emoji, selected, onSelect }) {
+  return (
+    <div className={`friend ${selected ? "selected" : ""}`}>
+      <span>{emoji}</span>
+      <span>{name}</span>
+      <span>SOME TEXT</span>
+      <button className="button" onClick={onSelect}>
+        {selected ? "close" : " select"}
+      </button>
     </div>
   );
 }
